411b03e1b90402c1d41f7d337536cee3b4bb8d47
Abhishek Tilwar <abhishektilwar@gmail.com>
2023-01-17 21:48:38 +0530

    Internship details

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,3 +149,26 @@ function onOpenHandler () {
     Instamojo.open('https://www.instamojo.com/@abhishektwar');
   });
   
+// try
+let nums = document.querySelectorAll(".nums .num");
+let section = document.querySelector(".three");
+let started = false; // Function Started ? No
+
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
